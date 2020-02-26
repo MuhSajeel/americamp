84635682d8b0d44490868e0835ce1443a13b86cb
Raheel Iqbal <raheeliqbal@attribes.com>
2020-02-26 15:31:16 +0500

    Header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-     <h1>Well come</h1>
+     <Header/>
+     <Footer/>
     </div>
   );
 }
