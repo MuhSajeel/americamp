dd2568157ad4c34f2878ac24aa424973b4f235bb
Muhammad Tehami <t3hami@gmail.com>
2020-03-03 11:02:23 +0500

    merged

--- src/components/HeaderImage/Header_Image.js
+++ src/components/HeaderImage/Header_Image.js
@@ -4,9 +4,9 @@ import './Header_Image.css';
 class Header extends Component {
     render() {
         return (
-            <div className="jumbotron big-banner mb-0 jumbotron-fluid" style={{ height: '630px', paddingTop: '150px' }}>
+            <div className="jumbotron big-banner mb-0 jumbotron-fluid">
                     <strong className="display-2 text-light">Welcome to AmeriCamp!</strong>
-                </div>
+            </div>
         );
     }
 }
